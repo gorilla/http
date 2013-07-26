a53abf50c036718468abd81bbd9ce86772e61ef2
Dave Cheney <dave@cheney.net>
2013-07-26 18:09:57 +1000

    Added 32bit dirty test.

--- client/client_test.go
+++ client/client_test.go
@@ -147,6 +147,7 @@ var responseContentLengthTests = []struct {
 	{"HTTP/1.0 200 OK\r\n\r\n ", -1},
 	{"HTTP/1.0 200 OK\r\nContent-Length: 1\r\n\r\n ", 1},
 	{"HTTP/1.0 200 OK\r\nContent-Length: 0\r\n\r\n", 0},
+	{"HTTP/1.0 200 OK\r\nContent-Length: 4294967296\r\n\r\n", 4294967296},
 }
 
 func TestResponseContentLength(t *testing.T) {
