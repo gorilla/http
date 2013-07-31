fedaf66c10d1d5f0604264dc1b362abcbf1ff3bf
Dave Cheney <dave@cheney.net>
2013-07-31 20:18:41 +1000

    fix example

--- example_test.go
+++ example_test.go
@@ -12,7 +12,7 @@ func ExampleGet() {
 	}
 }
 
-func ExampleClientPost() {
+func ExampleClient_Post() {
 	// send the contents of os.Stdin to a remote webserver.
 	status, _, r, err := DefaultClient.Post("http://example.com", nil, os.Stdin)
 	if err != nil {
