c2c2efaacd3703a6fd3f4b5a569bf8376f03f969
Dave Cheney <dave@cheney.net>
2013-07-31 19:20:55 +1000

    Fixed example

--- example_test.go
+++ example_test.go
@@ -1,16 +1,13 @@
 package http
 
 import (
-	"bytes"
+	"os"
 	"log"
 )
 
 func ExampleGet() {
-	var b bytes.Buffer
-	var url = "http://www.gorillatoolkit.org/"
-	_, err := Get(&b, url)
-	if err != nil {
+	// curl in 3 lines of code.
+	if _, err := Get(os.Stdout, "http://www.gorillatoolkit.org/") {
 		log.Fatalf("could not fetch: %v", err)
 	}
-	log.Printf("Fetched %v: %q", err, b.String())
 }
